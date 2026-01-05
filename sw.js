332ef12a87ab3d47dd06e8da5446d20de226a6d3
berny.hunter <bernyhunter@hotmail.com>
2026-01-05 21:44:18 +0100

    update sw pour logo versioning

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+const CACHE_VERSION = 'v2'; // change juste ce numéro quand tu veux forcer une mise à jour
+const CACHE_NAME = `sensitrack-cache-${CACHE_VERSION}`;
+
 const ASSETS = [
   './',
   './index.html',
@@ -8,3 +11,21 @@ const ASSETS = [
   './js/cycleComputer.js',
   './js/chartRenderer.js'
 ];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys
+          .filter(key => key !== CACHE_NAME)
+          .map(key => caches.delete(key))
+      )
+    )
+  );
+});
